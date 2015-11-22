2f2df747591b291a7ba709f496228e5fdfe3c239
Luis <Frost_Leviathan@hotmail.com>
2015-11-22 13:31:58 -0700

    Se agregaron las listas de reproduccion a la administracion.

---
+++ app/controllers/Playlists.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import models.Playlist;
+
+/**
+ * Este controlador es el encargador de manejar las listas de reproduccion de los usuarios.
+ *
+ * @author Javier Maldonado
+ */
+@CRUD.For(Playlist.class)
+public class Playlists extends Admin {
+
+}
